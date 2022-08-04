52bd99b7da2b252cf763fe010e606549e184b550
mijelpalcabello <miguel.castillo861999@gmail.com>
2022-08-04 11:58:46 -0500

    Agregando sección inicial de posts

---
+++ pages/posts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import  { Box, Heading } from '@chakra-ui/react'
+
+const Posts = () => {
+	return (
+		<Box m={6}>
+			<Heading>Todavía no hay posts</Heading>
+		</Box>
+	)
+}
+
+export default Posts
+
