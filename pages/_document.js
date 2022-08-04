66c4864498da214ea8a4059ab1b753e1cfb8b0fa
mijelpalcabello <miguel.castillo861999@gmail.com>
2022-08-04 11:54:28 -0500

    Agregando ícono a la página

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,9 @@ export default class Document extends NextDocument {
 	render() {
 		return (
 			<Html lang='en'>
-				<Head />
-
+				<Head>
+					<link rel='icon' href='/images/rosquilla.png' />
+				</Head>
 				<body>
 					<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 					<Main />
